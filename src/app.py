1ab21b5b2934e3e3e17c2e13721e4bd06dda8f52
Mirsha-RG <142954534+Mirsha-RG@users.noreply.github.com>
2025-08-06 23:45:41 +0000

    Se sube proyecto en SCR

--- src/app.py
+++ src/app.py
@@ -1,4 +1,45 @@
+from flask import Flask, request, render_template, jsonify
+import pickle
+import numpy as np
+
 from utils import db_connect
 engine = db_connect()
 
 # your code here
+app = Flask(__name__)
+
+
+# Cargar modelo
+with open('modelo.pkl', 'rb') as f:
+    modelo = pickle.load(f)
+
+# Cargar scaler
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predecir', methods=['POST'])
+def predecir():
+    datos = request.form
+
+    try:
+        # Extraer los valores del formulario
+        valores = [float(datos[var]) for var in [
+            'Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
+            'Insulin', 'BMI', 'Age', 'DiabetesPedigreeFunction'
+        ]]
+
+        entrada = np.array([valores])
+        entrada_esc = scaler.transform(entrada)
+        prediccion = modelo.predict(entrada_esc)[0]
+
+        return render_template('index.html', prediccion=prediccion)
+    
+    except Exception as e:
+        return f"Error en la predicción: {e}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
